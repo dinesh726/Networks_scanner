9f022fa2a1cb94adbb1329da16fea901fec0ff6d
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-04-05 13:16:01 +0530

    Update arp.py

--- arp.py
+++ arp.py
@@ -4,7 +4,10 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
-    arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered, unanswered = scapy.srp(arp_request_broadcast,timeout=1)
+    print(answered.summary())
+    print("unanswered",unanswered.summary())
 
 scan("192.168.12.1/24")
